c5825bf38fc1d12da4a8d8dc1dabf0f4821fc095
ctorres014 <ctorres014@gmail.com>
2019-02-20 18:44:33 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,20 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang="pug">
+  #app
+   station
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Station from './components/Station.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Station
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+  @import './assets/scss/main.scss';
+
 </style>
